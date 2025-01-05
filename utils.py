2a3a8df2d53f05d8c4b06b8ec18b91ba998019c6
Gabriel <gabriel.gracilianomb@gmail.com>
2025-01-05 12:00:33 -0300

    Refactor utils by reducing lines

--- utils.py
+++ utils.py
@@ -8,14 +8,12 @@ def get_file_data(file_name):
     return data
 
 def create_cipher(key_string):
-    key = key_string.encode()
-    return pyaes.AESModeOfOperationCTR(key)
+    return pyaes.AESModeOfOperationCTR(key_string.encode())
 
 def remove_file(file_name):
     os.remove(file_name)
 
 def save_changed_file(file_name, data):
-    new_file = file_name
-    new_file = open(f"{new_file}", "wb")
-    new_file.write(data)
-    new_file.close()
+    file_name = open(f"{file_name}", "wb")
+    file_name.write(data)
+    file_name.close()
